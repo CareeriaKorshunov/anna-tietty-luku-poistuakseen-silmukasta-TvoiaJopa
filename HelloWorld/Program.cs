c230ae729f34322754d3f76fc4086651ef7a28b1
Aretemii Korshunov <56575740+TvoiaJopa@users.noreply.github.com>
2024-09-24 10:31:38 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,40 @@
-﻿namespace HelloWorld
+namespace HelloWorld
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             //Koodi tähän
-           
-            
+            string syöte;
+            int luku;
+            while (true)
+            {
+
+                Console.Write("Kirjoita luku 4: ");
+                syöte = Console.ReadLine();
+
+                if (int.TryParse(syöte, out luku))
+                {
+                    if (luku == 4)
+                    {
+                        Console.WriteLine("Onnisuit! Poistuit silmukasta ");
+                        syöte = Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Yritäpä uudelleen..");
+                        syöte = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Et edes antanut lukua..");
+                    syöte = Console.ReadLine();
+                }
+                Console.WriteLine();
+
+            }
         }
     }
 }
